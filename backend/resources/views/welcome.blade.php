885a541347073d4e7368fc394b9ee208c4f800a2
Abinove Ramesh <145929189+abinoveramesh20@users.noreply.github.com>
2024-05-10 13:22:01 +0530

    Update welcome.blade.php

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>MyLaravel</title>
+        <title>My Laravel</title>
         
 
         <!-- Fonts -->
